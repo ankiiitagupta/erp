56600e070cd4e3a89b74c22a9f3c4350cdbbc0e9
Ankita Gupta <ankitagupta7470@gmail.com>
2024-11-26 15:07:33 +0530

    Revert ".."
    This reverts commit 17ade35c4c970458dbb842a7eb8aabe4587afcee.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,20 @@
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import Login from './Pages/Login';
 import FacultyDashboard from './faculty/components/facultyDashboard';
 import TodaysAttendance from './faculty/components/TodaysAttendance';
 import StudentDashboard from './Student/Components/StudentDashboard';
-import AdminDashboard from './Administration/Components/AdminDashboard';
+
 
 function App() {
+  
+
   return (
     <>
       <BrowserRouter>
-        {/* Optional Sidebar */}
+        {/* Sidebar will persist across all routes */}
         {/* <Sidebar /> */}
-
         <Routes>
           {/* Home Page */}
           <Route path="/" element={<Home />} />
@@ -23,7 +25,9 @@ function App() {
           {/* Dashboards */}
           <Route path="/facultydashboard/:FacultyID" element={<FacultyDashboard />} />
           <Route path="/todaysattendance" element={<TodaysAttendance />} />
-          <Route path="/AdminDashboard/:admin_id" element={<AdminDashboard />} />
+          <Route path="/studentdashboard/:RollNO" element={<StudentDashboard />} />
+                      {/* <Route path="/facultyreport" element={<FacultyReport />} /> */}
+
           <Route path="/studentdashboard/:RollNO" element={<StudentDashboard />} />
         </Routes>
       </BrowserRouter>
@@ -32,3 +36,6 @@ function App() {
 }
 
 export default App;
+
+
+
